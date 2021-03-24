3488d2c15f161fd94bd727051264e6a107adda95
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-03-24 16:25:58 -0300

    feat: header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Início</title>
+    </Head>
     <h1>
       Hello Word
     </h1>
+    </>
   )
 }

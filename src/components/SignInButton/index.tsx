659bcd5de1fdd3193dfda796d4f0b4e0724b7566
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-03-25 16:40:42 -0300

    feat: github auth signin

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,24 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
-    
-    return isUserLoggedIn ? (
+    const [session] = useSession();
+
+    return session ? (
         <button type="button" className={styles.signInButton}>
             <FaGithub color="#04d361" />
             Guilherme Orcezi
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>

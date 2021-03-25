d808862385cc17a276c650c76686baad6baf2f92
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-03-25 15:25:13 -0300

    feat: static site generator and stripe config

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,9 @@
 import styles from './styles.module.scss';
 
+interface SubscribeButtonProps{
+    priceId:string;
+}
+
 export function SubscribeButton(){
     return (
         <button type="button" className={styles.subscribeButton}>

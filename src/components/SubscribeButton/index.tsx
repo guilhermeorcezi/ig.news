8e90e7c54c2086c7c0be9dca8f3f1b8caf380763
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-04-07 13:03:25 -0300

    feat: first strip checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/client';
+import { useCallback } from 'react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,8 +9,34 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton(){
+    const [session] = useSession();
+
+    const handleSubscribe = useCallback(async () => {
+        if (!session){
+            signIn('github');
+            return;
+        }
+        console.log('ok')
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+        }catch(err){
+            alert(err.message)
+        }
+
+    },[]);
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button
+        type="button"
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     )

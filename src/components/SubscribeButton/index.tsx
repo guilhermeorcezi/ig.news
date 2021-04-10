2861fde5cb5c5e07bc1dc822854d6be3b609c988
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-04-10 13:01:47 -0300

    feat: session verify activeSubscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/dist/client/router';
 import { useCallback } from 'react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -10,10 +11,16 @@ interface SubscribeButtonProps{
 
 export function SubscribeButton(){
     const [session] = useSession();
+    const router = useRouter();
 
     const handleSubscribe = useCallback(async () => {
         if (!session){
             signIn('github');
+            return;
+        }
+
+        if (session.activeSubscription){
+            router.push('/posts');
             return;
         }
 
